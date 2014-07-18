a185bdb593bcf93730f8cc4590168c5024d7b0eb
Steven G. Harms <steven.harms@gmail.com>
2014-07-17 17:08:51 -0700

    Extract irregular_participles_retriever from Latinverb

--- Rakefile
+++ Rakefile
@@ -1,2 +1,12 @@
+require "rake/testtask"
 require "bundler/gem_tasks"
+
+task :default => :test
+
+Rake::TestTask.new do |t|
+  t.ruby_opts = [ '-rminitest/autorun', '-rminitest/pride', '-rlatinverb_irregular_participles_retriever' ]
+  t.libs << "test"
+  t.test_files = FileList['test/*test*.rb']
+  t.verbose = true
+end
 
--- lib/latinverb_irregular_participles_retriever.rb
+++ lib/latinverb_irregular_participles_retriever.rb
@@ -1,5 +1,19 @@
 require "latinverb_irregular_participles_retriever/version"
+require "latinverb_irregular_participles_retriever/supine_inferrer"
+require "latinverb_irregular_verb_definition_consumer"
 
-module LatinverbIrregularParticiplesRetriever
-  # Your code goes here...
+module Linguistics
+  module Latin
+    module Verb
+      class LatinVerb
+        class IrregularParticiplesRetriever < IrregularVerbDefinitionConsumer
+          def retrieve
+            participles_data = structure['participles']['data']
+            supine_data = SupineInferrer.new(participles_data["perfect_passive_participle"]).supine
+            OpenStruct.new(participles_data.merge({:supine => supine_data}))
+          end
+        end
+      end
+    end
+  end
 end
